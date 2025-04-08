96c436eb01441b36d22fca9be7d1763d5ba2347d
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-04-08 16:43:44 -0400

    Update vivado.sh

--- vivado.sh
+++ vivado.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v `pwd`:/home/user --workdir /home/user --entrypoint /opt/Xilinx/Vivado/2023.2/bin/vivado timkpaine/vivtest "$@"
+docker run -v `pwd`:/home/user --workdir /home/user --entrypoint /opt/Xilinx/Vivado/2023.2/bin/vivado -e LD_PRELOAD=/lib/x86_64-linux-gnu/libudev.so.1 timkpaine/vivtest "$@"
